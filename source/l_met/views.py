cdd18b991fe08cd7decce0379a1d86d4eecb23ce
Roman Pavlyuk <rpavlyuk@softserveinc.com>
2017-12-06 19:33:51 +0200

    Minor improvements

--- source/l_met/views.py
+++ source/l_met/views.py
@@ -13,17 +13,32 @@ import logging
 log = logging.getLogger(__name__)
 
 
+'''
+Processing file upload. All magic happens here!
+'''
+@view_config(route_name='upload_action', request_method='POST')
+def upload_action(request):
+
+    
+    return Response("Uploaded")
 
 
+'''
+Rendering (presenting) the file upload form
+'''
 @view_config(route_name='upload_form', request_method='GET')
 def upload_form(request):
+
     return render_to_response('templates/upload_form.pt',
-                              {'foo':1, 'bar':2},
+                              {},
                               request=request)
 
-
+'''
+Handling index view. Basically, this is just a placeholder, nothing here is so far
+'''
 @view_config(route_name='index', request_method='GET')
 def index(request):
+
     return Response("Index!")
     
     

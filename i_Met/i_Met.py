355c9f363776043110909e3376f4a6e5a3720f2a
Olga Kr <olichka121kr76@gmail.com>
2018-04-22 22:58:14 +0300

    Changing db structure, connecting front- to back-end

--- i_Met/i_Met.py
+++ i_Met/i_Met.py
@@ -1,8 +1,12 @@
-from flask import Flask, redirect, url_for, request, render_template, session
+from flask import Flask, redirect, url_for, request, render_template, session, flash
 from flask_pymongo import PyMongo
 import bcrypt
+from pymongo import MongoClient
 
 app = Flask(__name__)
+# client = MongoClient('localhost', 27017)    #Configure the connection to the database
+# db = client.i_Met
+
 
 app.config['MONGO_DBNAME'] = 'i-met'
 app.config['MONGO_URI'] = 'mongodb://Olga:olichka121@ds121289.mlab.com:21289/i-met'
@@ -12,43 +16,41 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'user' in session:
+        return render_template('index.html', url=url_for('user_cabinet'), name='Кабінет')
+    return render_template('index.html', url=url_for('login'), name='Увійти')
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    if 'username' in session:
-        return redirect(url_for('logout'))
+    if 'user' in session:
+        return redirect(url_for('user_cabinet'))
         # return 'You are logged as ' + session['username']
     if request.method == 'POST':
         user = mongo.db.users
-        login_user = user.find_one({'name': request.form['username']})
+        login_user = user.find_one({'email': request.form['email']})
         if login_user:
-            if bcrypt.hashpw(request.form['pass'].encode('utf-8'),
-                             login_user['password']) == login_user['password']:
-                session['username'] = request.form['username']
+            if bcrypt.hashpw(request.form['password'].encode(encoding='UTF-8', errors='strict'),
+                             login_user['password']) == \
+                    login_user['password']:
+                session['user'] = request.form['email']
                 return redirect(url_for('index'))
     return render_template('login.html')
-
-
-@app.route('/logout', methods=['GET', 'POST'])
-def logout():
-    if request.method == 'POST':
-        session.pop('username', None)
-        return redirect(url_for('index'))
-    return render_template('logout.html')
 
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
         users = mongo.db.users
-        existing_user = users.find_one({'name': request.form['username']})
+        existing_user = users.find_one({'email': request.form['email']})
 
         if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'name': request.form['username'], 'password': hashpass})
-            session['username'] = request.form['username']
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert(
+                {'email': request.form['email'], 'password': hashpass, 'name_surname': request.form['name_surname'],
+                 'account_num': {'3663434534': {'type': 'water',
+                                                'date': {'22,04,18': '100', '23,04,18': '150', '24,04,18': '200',
+                                                         '25,04,18': '280'}}}})
             return redirect(url_for('index'))
 
         return 'That email already exists'
@@ -56,22 +58,53 @@ def register():
     return render_template('register.html')
 
 
-@app.route('/')
-def logged_in():
-    if 'username' in session:
-        return 'You are logged in as ' + session['username']
-    return render_template('index.html')
+@app.route('/user')
+def user():
+    return render_template('user.html')
 
 
-@app.route('/chart/<name>')
-def chart(name):
-    users = mongo.db.users
-    user = users.find_one({'name': name})
-    labels = user['days']
-    values = user['days']
-    return render_template('chart.html', values=values, labels=labels)
+@app.route('/user_cabinet')
+def user_cabinet():
+    return render_template('user-cabinet.html')
+
+
+@app.route('/gas')
+def gas():
+    return render_template('gas.html')
+
+
+@app.route('/water')
+def water():
+    # if 'user' in session:
+        values = []
+        labels = []
+        users = mongo.db.users.find_one({'email': 'olichka121kr76@gmail.com'})
+        for dt in users['account_num']['3663434534']['date']:
+            labels.append(dt)
+        for dates in users['account_num']['3663434534']['date']:
+            values.append(users['account_num']['3663434534']['date'][dates])
+        return render_template('water.html', values=values, labels=labels)
+    # return render_template('water.html', values="", labels="")
+
+
+@app.context_processor
+def utility_processor():
+    def logout():
+        return session.pop('user', None)
+
+    return dict(logout=logout)
+
+
+@app.route('/chart')
+def chart():
+    if 'user' in session:
+        users = mongo.db.users
+        user = users.find_one({'email': 'user'})
+        labels = user['days']
+        values = user['days']
+        return render_template('chart.html', values=values, labels=labels)
 
 
 if __name__ == '__main__':
-    app.secret_key = 'secret'
+    app.secret_key = 'Smart_foolish_counter'
     app.run(debug=True)
